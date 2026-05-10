acd1c17dd2118e20106edd1dee72245191f07d84
yuna <yjeong1217@gmail.com>
2026-05-11 00:07:38 +0900

    Initial Commit

---
+++ poby/pobyApp.swift
@@ -0,0 +1,17 @@
+//
+//  pobyApp.swift
+//  poby
+//
+//  Created by arisoo on 5/11/26.
+//
+
+import SwiftUI
+
+@main
+struct pobyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
